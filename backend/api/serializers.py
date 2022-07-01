2288c3241c17f1b60e89318a28b758a691208230
Darian Ngo <ngodarian@gmail.com>
2022-07-01 14:08:12 -0700

    non reg serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,14 @@
+from dataclasses import fields
 from rest_framework import serializers
 from event.models.event import Event
+from event.models.event import NonRegisteredUser
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
+        fields = '__all__'
+
+class NonRegisteredUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NonRegisteredUser
         fields = '__all__'

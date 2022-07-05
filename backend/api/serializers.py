ab018c42794dcdcc383230fbd83c144f1fea07d9
tdslinden <linden-k@hotmail.com>
2022-07-04 21:32:52 -0700

    Add user model serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,14 @@
 from dataclasses import fields
 from rest_framework import serializers
 from event.models.event import Event
+from event.models.user import User
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
         fields = '__all__'

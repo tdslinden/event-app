2894a350a8e0b50ce6a5a3be48b4ec7e0dc20db7
tdslinden <linden-k@hotmail.com>
2022-07-12 22:20:14 -0700

    MAke username the primary key of the user model

--- backend/event/models/user.py
+++ backend/event/models/user.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
 
+
 class User(AbstractUser):
+    username = models.CharField(max_length=150, primary_key=True)
     date_of_birth = models.DateTimeField()
     phone_number = PhoneNumberField()

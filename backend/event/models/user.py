31b08ece5625cd22ae42c6e00ebb28eae0673619
tdslinden <linden-k@hotmail.com>
2022-07-12 22:28:48 -0700

    Force certain User model values to be required

--- backend/event/models/user.py
+++ backend/event/models/user.py
@@ -5,5 +5,8 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class User(AbstractUser):
     username = models.CharField(max_length=150, primary_key=True)
+    first_name = models.CharField(max_length=150, blank=False)
+    last_name = models.CharField(max_length=150, blank=False)
+    email = models.EmailField(max_length=254, blank=False)
     date_of_birth = models.DateTimeField()
-    phone_number = PhoneNumberField()
+    phone_number = PhoneNumberField(blank=False)

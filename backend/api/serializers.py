dbf93b36610b2758c22a470c412f7f96f5a12a5d
tdslinden <linden-k@hotmail.com>
2022-08-04 22:21:05 -0700

    Add the url and serializers for the registered users api

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,15 +1,22 @@
-from dataclasses import fields
 from rest_framework import serializers
-from event.models.event import Event
+# from event.models.event import Event
 from event.models.non_registered import NonRegisteredUser
+from event.models import RegisteredUser, Event
+
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
+
+
 class NonRegisteredUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = NonRegisteredUser
         fields = '__all__'
 
-        
+
+class RegisteredUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RegisteredUser
+        fields = '__all__'

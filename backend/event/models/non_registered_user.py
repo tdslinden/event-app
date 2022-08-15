6fd199b57190f396c5c0f1e6cb80c43b655c945e
tdslinden <linden-k@hotmail.com>
2022-08-15 13:58:31 -0700

    Remove event foreign key from non_registered_user.py

--- backend/event/models/non_registered_user.py
+++ backend/event/models/non_registered_user.py
@@ -1,11 +1,9 @@
-from pyexpat import model
 from django.db import models
-from .event import Event
 from phonenumber_field.modelfields import PhoneNumberField
+
 
 class NonRegisteredUser(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
     phone_number = PhoneNumberField()
-    event = models.ForeignKey(to=Event, on_delete=models.CASCADE)
